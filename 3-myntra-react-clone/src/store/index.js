6ca0224060af5947d2d28006338b21b04cc4852d
pathareshubham98-dot <pathareshubham98@gmail.com>
2026-08-21 18:07:16 +0530

    bag page item consums

--- 3-myntra-react-clone/src/store/index.js
+++ 3-myntra-react-clone/src/store/index.js
@@ -1,13 +1,9 @@
 import {configureStore} from "@reduxjs/toolkit";
 import itemsSlice from "./itemsSlice";
-import fetchStatusSlice from "./fetchStatusSlice";
-import bagSlice from "./bagSlice";
 
 const myntraStore = configureStore({
   reducer: {
-    items: itemsSlice.reducer,
-    fetchStatus: fetchStatusSlice.reducer,
-    bag: bagSlice.reducer,
+    items: itemsSlice.reducer
   }
 });
 
--- 3-myntra-react-clone/src/store/itemsSlice.js
+++ 3-myntra-react-clone/src/store/itemsSlice.js
@@ -1,11 +1,12 @@
 import {createSlice} from "@reduxjs/toolkit";
+import { DEFAULT_ITEMS } from "../data/items";
 
 const itemsSlice = createSlice({
   name: 'items',
-  initialState: [],
+  initialState:DEFAULT_ITEMS,
   reducers: {
     addInitialItems: (state, action) => {
-      return action.payload;
+      return store;
     }
   }
 });
@@ -13,3 +14,4 @@ const itemsSlice = createSlice({
 export const itemsActions = itemsSlice.actions;
 
 export default itemsSlice;
+
