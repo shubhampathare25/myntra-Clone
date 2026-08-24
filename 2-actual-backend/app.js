2616443d2a1cf938a4e247ccd3500744ac4aaa1f
pathareshubham98-dot <pathareshubham98@gmail.com>
2026-08-24 23:38:27 +0530

    fix route issue

--- 2-actual-backend/app.js
+++ 2-actual-backend/app.js
@@ -14,6 +14,12 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use('/', async (req, res) => {
+  const storedItems = await getStoredItems();
+  await new Promise((resolve, reject) => setTimeout(() => resolve(), 2000));
+  res.json({ items: storedItems });
+})
+
 app.get('/items', async (req, res) => {
   const storedItems = await getStoredItems();
   await new Promise((resolve, reject) => setTimeout(() => resolve(), 2000));
