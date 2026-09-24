fb2a0c441a3723c021753502a9ef64a19a64ee30
pathareshubham98-dot <pathareshubham98@gmail.com>
2026-09-24 23:24:37 +0530

    Update Header component with login dropdown

--- 3-myntra-react-clone/src/components/Header.jsx
+++ 3-myntra-react-clone/src/components/Header.jsx
@@ -1,14 +1,17 @@
+import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { searchActions } from "../store/searchSlice";
 import { Link } from "react-router-dom";
 import { CgProfile } from "react-icons/cg";
 import { IoHeart } from "react-icons/io5";
 import { RiShoppingBagFill } from "react-icons/ri";
+import { FaUser, FaShieldAlt } from "react-icons/fa";
 
 const Header = () => {
   const dispatch = useDispatch();
   const bag = useSelector((store) => store.bag || []);
   const wishlist = useSelector((store) => store.wishlist || []);
+  const [showDropdown, setShowDropdown] = useState(false);
 
   const handleSearchChange = (e) => {
     dispatch(searchActions.setSearchQuery(e.target.value));
@@ -47,17 +50,88 @@ const Header = () => {
       </div>
 
       <div className="action_bar">
-        {}
-        <Link
-          to="/"
+        <div
           className="action_container"
-          style={{ textDecoration: "none", color: "black" }}
+          style={{ position: "relative", cursor: "pointer" }}
+          onClick={() => setShowDropdown(!showDropdown)}
         >
-          <CgProfile style={{ fontSize: "22px" }} />
-          <span className="action_name">Profile</span>
-        </Link>
+          <div
+            style={{
+              textDecoration: "none",
+              color: "black",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <CgProfile style={{ fontSize: "22px" }} />
+            <span className="action_name">Login</span>
+          </div>
 
-        {}
+          {showDropdown && (
+            <div
+              style={{
+                position: "absolute",
+                top: "100%",
+                right: 0,
+                backgroundColor: "white",
+                boxShadow: "0px 4px 12px rgba(0,0,0,0.15)",
+                borderRadius: "4px",
+                width: "180px",
+                padding: "10px 0",
+                zIndex: 1000,
+                textAlign: "left",
+              }}
+              onClick={(e) => e.stopPropagation()}
+            >
+              <div
+                style={{
+                  padding: "8px 15px",
+                  fontSize: "11px",
+                  fontWeight: "bold",
+                  color: "#333",
+                  borderBottom: "1px solid #f5f5f5",
+                }}
+              >
+                SIGN IN TO MYNTRA-CLONE
+              </div>
+              <Link
+                to="/login?type=user"
+                onClick={() => setShowDropdown(false)}
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  gap: "10px",
+                  padding: "10px 15px",
+                  color: "#282c3f",
+                  textDecoration: "none",
+                  fontSize: "14px",
+                }}
+              >
+                <FaUser style={{ fontSize: "12px", color: "#535766" }} /> User
+                Login
+              </Link>
+
+              <Link
+                to="/admin/login"
+                onClick={() => setShowDropdown(false)}
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  gap: "10px",
+                  padding: "10px 15px",
+                  color: "#282c3f",
+                  textDecoration: "none",
+                  fontSize: "14px",
+                }}
+              >
+                <FaShieldAlt style={{ fontSize: "12px", color: "#535766" }} />{" "}
+                Admin Login
+              </Link>
+            </div>
+          )}
+        </div>
+
         <Link
           to="/wishlist"
           className="action_container"
@@ -70,7 +144,6 @@ const Header = () => {
           )}
         </Link>
 
-        {}
         <Link
           to="/bag"
           className="action_container"
